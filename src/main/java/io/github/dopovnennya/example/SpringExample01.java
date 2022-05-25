24d697890072f4244d39a2c94cd8a9f43b3a4421
dopovnennya <106244535+dopovnennya@users.noreply.github.com>
2022-05-25 13:54:44 +0200

    initial commit

---
+++ src/main/java/io/github/dopovnennya/example/SpringExample01.java
@@ -0,0 +1,12 @@
+package io.github.dopovnennya.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringExample01 {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringExample01.class, args);
+    }
+}
